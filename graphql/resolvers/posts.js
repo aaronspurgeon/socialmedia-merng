c7427fa9e4027a9693287dbdb48a648102351140
aaronspurgeon <aaronspurgeoniscool@gmail.com>
2020-10-26 20:21:18 -0400

    Adding functionality to create a new post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
-const Post = require("../../Post")
+const Post = require("../../Post");
+const checkAuth = require('../../util/checkAuth');
 
 module.exports = {
     Query: {
@@ -10,5 +11,34 @@ module.exports = {
             throw new Error(err);
           }
         },
+        async getPost(_, {postId}) {
+          try{
+            const post = await Post.findById(postId)
+            if(post) {
+              return post;
+            } else {
+              throw new Error("Post not found");
+            }
+          } catch(err) {
+            throw new Error(err)
+          }
+        }
       },
+      Mutation: {
+        async createPost(_, {body}, context) {
+          const user = checkAuth(context);
+          console.log(user);
+
+          const newPost = new Post({
+            body,
+            user: user.id,
+            username: user.username,
+            createdAt: new Date().toISOString()
+          });
+
+          const post = await newPost.save();
+
+          return post;
+        }
+      }
 }
