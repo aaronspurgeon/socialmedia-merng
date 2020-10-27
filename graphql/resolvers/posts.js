8a57c4e57304401c99771c7089824554ffd2a67d
aaronspurgeon <aaronspurgeoniscool@gmail.com>
2020-10-26 20:59:50 -0400

    Added a way for users to like and comment on posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../Post");
 const checkAuth = require('../../util/checkAuth');
-const {AuthenticationError} = require('apollo-server');
+const {AuthenticationError, UserInputError} = require('apollo-server');
 
 
 module.exports = {
@@ -57,6 +57,27 @@ module.exports = {
           } catch(err) {
             throw new Error(err);
           }
+        },
+        async likePost(_, {postId}, context) {
+          const {username} = checkAuth(context);
+
+          const post = await Post.findById(postId);
+
+          if(post) {
+            if(post.likes.find(like => like.username === username)){
+              // Post already likes, unlike it
+              post.likes = post.likes.filter(like => like.username !== username);
+              
+            } else {
+              // not liked, like post
+              post.likes.push({
+                username,
+                createdAt: new Date().toISOString()
+              })
+            }
+            await post.save()
+            return post;
+          } else throw new UserInputError('Post not found');
         }
       }
 }
